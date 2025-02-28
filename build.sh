b6314d79685a77670615e68cfa899c9fed3c3e70
Grégoire Compagnon <obeone@obeone.org>
2025-02-28 06:56:25 +0100

    feat(build.sh): introduce BUILDER variable and cache configuration
    This commit introduces a BUILDER variable to streamline the build process by allowing easy switching of builders. Additionally, it configures Docker Buildx with caching options for improved performance by using a private registry as both a source and destination for build caches.

--- build.sh
+++ build.sh
@@ -1,48 +1,59 @@
 #!/usr/bin/env bash
 set -e
 
+BUILDER=kube-multiplatform
+
 # Base
 docker buildx build \
-    --builder cloud-obeoneorg-cloud \
+    --builder $BUILDER \
     --platform linux/amd64,linux/arm64 \
     -t obeoneorg/netshoot:latest \
+    --cache-from type=registry,ref=private.registry-cache.obeone.org/netshoot:build \
+    --cache-to type=registry,ref=private.registry-cache.obeone.org/netshoot:build,mode=max \
     --target base \
     --push \
     .
 
 # Docker
 docker buildx build \
-    --builder cloud-obeoneorg-cloud \
+    --builder $BUILDER \
     --platform linux/amd64,linux/arm64 \
     -t obeoneorg/netshoot:docker \
+    --cache-from type=registry,ref=private.registry-cache.obeone.org/netshoot:build \
+    --cache-to type=registry,ref=private.registry-cache.obeone.org/netshoot:build,mode=max \
     --target docker \
     --push \
     .
 
 # nerdctl
 docker buildx build \
-    --builder cloud-obeoneorg-cloud \
+    --builder $BUILDER \
     --platform linux/amd64,linux/arm64 \
     -t obeoneorg/netshoot:nerdctl \
+    --cache-from type=registry,ref=private.registry-cache.obeone.org/netshoot:build \
+    --cache-to type=registry,ref=private.registry-cache.obeone.org/netshoot:build,mode=max \
     --target nerdctl \
     --push \
     .
 
 # nerdctl
 docker buildx build \
-    --builder cloud-obeoneorg-cloud \
+    --builder $BUILDER \
     --platform linux/amd64,linux/arm64 \
     -t obeoneorg/netshoot:nerdctl \
+    --cache-from type=registry,ref=private.registry-cache.obeone.org/netshoot:build \
+    --cache-to type=registry,ref=private.registry-cache.obeone.org/netshoot:build,mode=max \
     --target nerdctl \
     --push \
     .
 
 # podman
 docker buildx build \
-    --builder cloud-obeoneorg-cloud \
+    --builder $BUILDER \
     --platform linux/amd64,linux/arm64 \
     -t obeoneorg/netshoot:podman \
+    --cache-from type=registry,ref=private.registry-cache.obeone.org/netshoot:build \
+    --cache-to type=registry,ref=private.registry-cache.obeone.org/netshoot:build,mode=max \
     --target podman \
     --push \
     .
-
